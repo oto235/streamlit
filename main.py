68381b5d420283ad200a2fbc06ae28c7ae7b286b
oto235 <oto235@gmail.com>
2024-10-31 13:34:20 -0500

    here

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://kinnser.net/login.cfm")
+userElem = driver.find_element(By.ID, "username")
+userElem.send_keys("jason")
+
+here = 6
